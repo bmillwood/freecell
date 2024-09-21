c1ad743607d80c619e464bfc78115a3cdedff60a
Ben Millwood <thebenmachine+git@gmail.com>
2024-09-21 20:51:14 +0100

    vendor a filtered registry.dat

--- default.nix
+++ default.nix
@@ -15,10 +15,9 @@ stdenv.mkDerivation {
   configurePhase = pkgs.elmPackages.fetchElmDeps {
     elmPackages = import ./elm-srcs.nix;
     elmVersion = "0.19.1";
-    registryDat = ~/.elm/0.19.1/packages/registry.dat;
+    registryDat = ./registry.dat;
   };
   buildPhase = ''
-    wc -c $ELM_HOME/0.19.1/packages/registry.dat
     mkdir "$out"
     cp index.html "$out/"
     elm make --output="$out/elm.js" src/Main.elm
